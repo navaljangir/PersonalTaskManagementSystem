0eb57f2d9b6754ad908c469a8104d6cd064e954f
NAVAL JANGIR <jangirnaval392003@gmail.com>
2025-02-08 02:17:14 +0530

    mend

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,23 @@
 import { NextResponse } from 'next/server';
-import {withAuth} from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware';
+
 export const config = {
     matcher: ['/tasks', '/projects'],
 };
 
-export default withAuth(async(req)=>{
-    const token = req.nextauth.token
-    if(!token){
-        return NextResponse.redirect(new URL('/invalidsession' , req.url))
+export default withAuth(async (req) => {
+    const token = req.nextauth.token;
+
+    if (!token) {
+        return NextResponse.next();
     }
-    const user = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user?token=${token.jwtToken}`)
-    const userExists = await user.json()
-    if(!userExists.user){
-        return NextResponse.redirect(new URL('/invalidsession' , req.url))
+    const userResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/user?token=${token.jwtToken}`
+    );
+    const userExists = await userResponse.json();
+    if (!userExists.user) {
+        return NextResponse.redirect(new URL('/invalidsession', req.url));
     }
-    return NextResponse.next()
-    console.log(token)
-})
+
+    return NextResponse.next();
+});
